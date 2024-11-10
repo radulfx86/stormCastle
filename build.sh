5ac69c913509b338911b8d1e495df7d10e814e17
Frank Ehmann <frank.ehmann@gmail.com>
2024-11-10 23:33:58 +0100

    fix build script for HTML build

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
     -sFULL_ES3=1 \
     -sWASM=0 \
     src/main.cpp \
+    src/ecs.cpp \
+    src/systems.cpp \
+    src/object.cpp \
     --preload-file shaders \
     --preload-file assets/images/characters.png \
     --use-preload-plugins \
